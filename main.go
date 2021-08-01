25c79507e6685c98c7196d9acdc0a031b70695a4
Arran France <arran@arranfrance.com>
2021-08-01 02:36:52 +0100

    add: more parameters to sqip-ffi

--- main.go
+++ main.go
@@ -6,16 +6,12 @@ import (
 )
 
 //export MakeSVG
-func MakeSVG(path string) *C.char {
+func MakeSVG(path string, numberOfPrimitives int, mode int, alpha int, workers int) *C.char {
 	workSize := 256  // large images get resized to this - higher size grants no boons
-	count := 8       // number of primitive SVG shapes
-	mode := 1        // shape type
-	alpha := 128     // alpha value
 	repeat := 0      // add N extra shapes each iteration with reduced search (mostly good for beziers)
-	workers := 1     // number of parallel workers
 	background := "" // background color (hex)
 
-	svg, _, _, err := sqip.Run(path, workSize, count, mode, alpha, repeat, workers, background)
+	svg, _, _, err := sqip.Run(path, workSize, numberOfPrimitives, mode, alpha, repeat, workers, background)
 	if err != nil {
 		result := "Error " + err.Error()
 		return C.CString(result)
